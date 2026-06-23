24145194892bbbda5be2097ccde0de362f51b88a
Rohan Giri <giri.dev.rohan@gmail.com>
2026-06-24 03:40:15 +0530

    modified the functions and the route for better code genration

--- src/app/api/urls/route.ts
+++ src/app/api/urls/route.ts
@@ -1,25 +1,27 @@
 import { NextRequest, NextResponse } from "next/server";
 import { prisma } from "../../../../lib/prisma";
 import { URL } from "node:url";
-import { error } from "node:console";
 
 function isValidUrl(url: string) : boolean{
     try {
         new URL(url)
         return true;  
-    } catch (error) {
+    } catch {
         return false;
     }
 }
 
 // function to geneate random code
-function generateCode(): string{
-    return Math.random().toString(30).slice(2, 10)
-    /*
-    How it works:
-    Math.random() → generates a random decimal like 0.735821...
-    .toString(36) → converts it to base-36 (digits 0-9 + letters a-z), giving something like "0.k7x2mq9p4r"
-    .slice(2, 10) → removes the "0." prefix and takes 8 chars */
+function generateCode(originalUrl : string): string{
+    const parsedUrl = new URL(originalUrl)
+
+    const urlPath = `${parsedUrl.hostname}${parsedUrl.pathname}`
+                    .replace(/[^a-zA-Z0-9]/g, "")
+                    .toLowerCase()
+                    .slice(0,4)
+    const randomPart = Math.random().toString(36).slice(2,10)
+
+    return `${randomPart}${urlPath}`
 }
 
 // create a short url 
@@ -32,17 +34,32 @@ export async function POST(req: NextRequest){
             return NextResponse.json({error: "Invalid url"}, {status: 400})
         }
     
-        const randomCode = generateCode()
+        let randomCode = generateCode(originalUrl)
+        let attempts = 0;
     
-        const shortCodeDb = await prisma.url.findFirst({
+        let shortCodeDb = await prisma.url.findUnique({
             where: {
                 shortCode: randomCode
             }
         })
     
-        if(shortCodeDb){
-            return NextResponse.json({error: "Short code already exits"}, {status: 500})
+        while(shortCodeDb && attempts < 5){
+            randomCode = generateCode(originalUrl)
+            attempts++;
+    
+            shortCodeDb = await prisma.url.findUnique({
+                where: {
+                    shortCode: randomCode
+                }
+        })
         }
+
+        if (shortCodeDb) {
+            return NextResponse.json(
+                { error: "Could not generate a unique short code" },
+                { status: 500 }
+            )
+}
     
         const newUrlCode = await prisma.url.create({
             data: {
@@ -51,9 +68,13 @@ export async function POST(req: NextRequest){
             }
         })
     
-        return NextResponse.json(newUrlCode, {status: 400})
+        return NextResponse.json(newUrlCode, {status: 201})
     } catch (error) {
         console.error("Failed to create short code", error)
+        return NextResponse.json(
+            { error: "Failed to create short URL" },
+            { status: 500 }
+  )
     }
 }
 
